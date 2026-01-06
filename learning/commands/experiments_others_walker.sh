27de102d3be118ab61ad1293cec0074bc8e331e4
sukchul <tjrcjf410@snu.ac.kr>
2026-01-06 12:52:56 +0900

    asdf

--- learning/commands/experiments_others_walker.sh
+++ learning/commands/experiments_others_walker.sh
@@ -1,16 +1,16 @@
-wandb_project="walker-sampler2"
+wandb_project="walker-sampler3"
 
-for seed in 2 3 4
+for seed in 0 1 2 3 4
 do
-python train.py policy=ppo task=WalkerWalk wandb_project=$wandb_project seed=$seed
-python train.py policy=ppo_nodr task=WalkerWalk wandb_project=$wandb_project seed=$seed
+python run.py policy=ppo task=WalkerWalk wandb_project=$wandb_project seed=$seed
+python run.py policy=ppo_nodr task=WalkerWalk wandb_project=$wandb_project seed=$seed
 done
 for success_threshold in 0.65 0.7
 do
-    python train.py policy=adrppo wandb_project=$wandb_project  task=WalkerWalk success_threshold=$success_threshold
+    python run.py policy=adrppo wandb_project=$wandb_project  task=WalkerWalk success_threshold=$success_threshold
     for success_rate_condition in 0.6 0.7 0.8
     do
-       python train.py policy=doraemonppo wandb_project=$wandb_project success_threshold=$success_threshold task=WalkerWalk success_rate_condition=$success_rate_condition
+       python run.py policy=doraemonppo wandb_project=$wandb_project success_threshold=$success_threshold task=WalkerWalk success_rate_condition=$success_rate_condition
     done
 done
 
