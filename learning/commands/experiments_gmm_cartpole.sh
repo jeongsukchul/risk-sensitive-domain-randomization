3eec644b8b23082f6bcb02c598f2993e51125f4b
sukchul <tjrcjf410@snu.ac.kr>
2026-01-12 16:08:02 +0900

    ewnfjsdoi

--- learning/commands/experiments_gmm_cartpole.sh
+++ learning/commands/experiments_gmm_cartpole.sh
@@ -1,7 +1,14 @@
 # wandb_project="walkerwalk-sampler"
 wandb_project="cartpole-sampler6"
 
-for beta in -30 -20 -10 -5 -2 -1 0 1 2 5 10
+for beta in 5
+    do
+    for seed in 2 3 4
+        do
+        python run.py policy=gmmppo beta=$beta wandb_project=$wandb_project task=CartpoleSwingupSparse seed=$seed sampler_update_freq=1
+        done
+    done
+for beta in 10
     do
     for seed in 0 1 2 3 4
         do
--- learning/commands/experiments_others_cartpole.sh
+++ learning/commands/experiments_others_cartpole.sh
@@ -1,10 +1,10 @@
 task=CartpoleSwingupSparse
 wandb_project="cartpole-sampler6"
-for seed in 5
-do
-python run.py policy=ppo task=$task wandb_project=$wandb_project seed=$seed
-python run.py policy=ppo_nodr task=$task wandb_project=$wandb_project seed=$seed
-done
+# for seed in 5
+# do
+# python run.py policy=ppo task=$task wandb_project=$wandb_project seed=$seed
+# python run.py policy=ppo_nodr task=$task wandb_project=$wandb_project seed=$seed
+# done
 success_threshold= 0.7
 success_rate_condition= 0.8
 for seed in 0 1 2 3 4
