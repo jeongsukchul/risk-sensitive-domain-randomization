0e7667c1ad9173e37faddc320e955803dab6d57b
sukchul <tjrcjf410@snu.ac.kr>
2026-01-12 12:28:05 +0900

    asdf

--- learning/commands/experiments_gmm_cartpole.sh
+++ learning/commands/experiments_gmm_cartpole.sh
@@ -1,10 +1,10 @@
 # wandb_project="walkerwalk-sampler"
-wandb_project="cartpole-sampler3"
+wandb_project="cartpole-sampler6"
 
 for beta in -30 -20 -10 -5 -2 -1 0 1 2 5 10
     do
     for seed in 0 1 2 3 4
         do
-        python run.py policy=gmmppo beta=$beta wandb_project=$wandb_project task=CartpoleSwingup seed=$seed sampler_update_freq=1
+        python run.py policy=gmmppo beta=$beta wandb_project=$wandb_project task=CartpoleSwingupSparse seed=$seed sampler_update_freq=1
         done
     done
--- learning/commands/experiments_others_cartpole.sh
+++ learning/commands/experiments_others_cartpole.sh
@@ -1,15 +1,15 @@
-wandb_project="cartpole-sampler3"
+task=CartpoleSwingupSparse
+wandb_project="cartpole-sampler6"
+for seed in 5
+do
+python run.py policy=ppo task=$task wandb_project=$wandb_project seed=$seed
+python run.py policy=ppo_nodr task=$task wandb_project=$wandb_project seed=$seed
+done
+success_threshold= 0.7
+success_rate_condition= 0.8
 for seed in 0 1 2 3 4
 do
-python run.py policy=ppo task=CartpoleSwingup wandb_project=$wandb_project seed=$seed
-python run.py policy=ppo_nodr task=CartpoleSwingup wandb_project=$wandb_project seed=$seed
-done
-for success_threshold in 0.65 0.7
-do
-    python run.py policy=adrppo wandb_project=$wandb_project  task=CartpoleSwingup success_threshold=$success_threshold
-    for success_rate_condition in 0.6 0.7 0.8
-    do
-       python run.py policy=doraemonppo wandb_project=$wandb_project success_threshold=$success_threshold task=CartpoleSwingup success_rate_condition=$success_rate_condition
-    done
+    python run.py policy=adrppo wandb_project=$wandb_project  task=$task success_threshold=$success_threshold seed=$seed
+    python run.py policy=doraemonppo wandb_project=$wandb_project success_threshold=$success_threshold task=$task success_rate_condition=$success_rate_condition seed=$seed
 done
 
