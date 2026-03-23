d8f4ba1e06a601c4ab2eabb8de241710faa0a97f
sukchul <tjrcjf410@snu.ac.kr>
2026-03-23 12:40:05 +0900

    sef

--- learning/commands/exp_push.sh
+++ learning/commands/exp_push.sh
@@ -8,6 +8,6 @@ for beta in -30 -20 -10 0 5 10
     do
     for seed in 0 1 2 3 4 5 6 7
         do
-        python run.py policy=gbsppo beta=$beta wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed sampler_update_freq=1 n_sampler_iters=1
+        python run.py policy=gbsppo beta=$beta wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed sampler_update_freq=1 n_sampler_iters=1 impl=warp
         done
     done
