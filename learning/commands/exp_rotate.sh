747e6c3aa2f224ce1e3febec07a7e1e2d48dac03
sukchul <tjrcjf410@snu.ac.kr>
2026-03-24 18:47:55 +0900

    asef

---
+++ learning/commands/exp_rotate.sh
@@ -0,0 +1,14 @@
+wandb_project="rotate-sampler"
+for seed in 0 1 2 3 4 5 6 7
+    do
+    python run.py policy=ppo wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed sampler_update_freq=1 n_sampler_iters=1 impl=warp
+    python run.py policy=gbsppo beta=-20 wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed sampler_update_freq=1 n_sampler_iters=1 impl=warp
+    done
+
+for beta in -30 -10 0 5 10
+    do
+    for seed in 0 1 2 3 4 5 6 7
+        do
+        python run.py policy=gbsppo beta=$beta wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed sampler_update_freq=1 n_sampler_iters=1 impl=warp
+        done
+    done
