6acd9366bdfd3be85069920f318476d3dc37b582
tjrcjf410 <tjrcjf410@snu.ack.r>
2026-03-25 13:19:03 +0900

    asf

--- learning/commands/exp_leap.sh
+++ learning/commands/exp_leap.sh
@@ -1,14 +1,14 @@
 wandb_project="leap-sampler"
 for seed in 0 1 2 3 4 5 6 7
     do
-    python run.py policy=ppo wandb_project=$wandb_project task=LeapCubeReorient seed=$seed impl=jax
-    python run.py policy=gbsppo beta=-20 wandb_project=$wandb_project task=LeapCubeReorient seed=$seed impl=jax
+    python run.py policy=ppo wandb_project=$wandb_project task=LeapCubeReorient seed=$seed impl=warp
+    python run.py policy=gbsppo beta=-20 wandb_project=$wandb_project task=LeapCubeReorient seed=$seed impl=warp
     done
 
 for beta in -30 -10 0 5 10
     do
     for seed in 0 1 2 3 4 5 6 7
         do
-        python run.py policy=gbsppo beta=$beta wandb_project=$wandb_project task=LeapCubeReorient seed=$seed impl=jax
+        python run.py policy=gbsppo beta=$beta wandb_project=$wandb_project task=LeapCubeReorient seed=$seed impl=warp
         done
     done
