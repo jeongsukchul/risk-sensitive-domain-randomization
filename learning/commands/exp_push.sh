2a79bf13af5afc4b8dadc0e2460ba1eb593d6b35
tjrcjf410 <tjrcjf410@snu.ac.kr>
2026-03-27 01:21:48 +0900

    adsf

--- learning/commands/exp_push.sh
+++ learning/commands/exp_push.sh
@@ -2,7 +2,7 @@ wandb_project="push-sampler"
 for seed in 0 1 2 3 4 5 6 7
     do
     python run.py policy=ppo wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed impl=warp
-    python run.py policy=gbsppo beta=-20 wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed impl=warp
+    python run.py policy=gmmppo beta=-20 wandb_project=$wandb_project task=PandaRobotiqPushCube seed=$seed impl=warp
     done
 
 for beta in -30 -10 0 5 10
