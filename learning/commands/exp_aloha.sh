001e3c7853753bdd72fed408b6ca8dc14eaf2ba2
sukchul <tjrcjf410@snu.ac.kr>
2026-03-27 09:27:41 +0900

    asdf

--- learning/commands/exp_aloha.sh
+++ learning/commands/exp_aloha.sh
@@ -1,13 +1,13 @@
 wandb_project="aloha-sampler"
 for seed in 0 1 2 3 4 5 6 7
     do
-    python run.py policy=ppo impl=warp wandb_project=$wandb_project impl=warp task=AlohaSinglePegInsertion seed=$seed 
-    python run.py policy=gmmppo impl=warp beta=-20 wandb_project=$wandb_project impl=warp task=AlohaSinglePegInsertion seed=$seed 
+    python run.py policy=ppo impl=warp wandb_project=$wandb_project impl=warp task=AlohaSinglePegInsertion seed=$seed sampler_visualization=true
+    python run.py policy=gmmppo impl=warp beta=-20 wandb_project=$wandb_project impl=warp task=AlohaSinglePegInsertion seed=$seed  sampler_visualization=true
     done
 for beta in -30 -10 0 5 10
     do
     for seed in 0 1 2 3 4 5 6 7
         do
-        python run.py policy=gbsppo impl=warp beta=$beta wandb_project=$wandb_project impl=warp task=AlohaSinglePegInsertion seed=$seed 
+        python run.py policy=gbsppo impl=warp beta=$beta wandb_project=$wandb_project impl=warp task=AlohaSinglePegInsertion seed=$seed sampler_visualization=true
         done
     done
--- learning/commands/exp_rotate.sh
+++ learning/commands/exp_rotate.sh
@@ -1,14 +1,14 @@
 wandb_project="rotate-sampler"
 for seed in 0 1 2 3 4 5 6 7
     do
-    python run.py policy=ppo wandb_project=$wandb_project task=LeapCubeRotateZAxis seed=$seed impl=warp
-    python run.py policy=gbsppo beta=-20 wandb_project=$wandb_project task=LeapCubeRotateZAxis seed=$seed impl=warp
+    python run.py policy=ppo wandb_project=$wandb_project task=LeapCubeRotateZAxis seed=$seed impl=warp sampler_visualization=true
+    python run.py policy=gbsppo beta=-20 wandb_project=$wandb_project task=LeapCubeRotateZAxis seed=$seed impl=warp sampler_visualization=true
     done
 
 for beta in -30 -10 0 5 10
     do
     for seed in 0 1 2 3 4 5 6 7
         do
-        python run.py policy=gbsppo beta=$beta wandb_project=$wandb_project task=LeapCubeRotateZAxis seed=$seed impl=warp
+        python run.py policy=gbsppo beta=$beta wandb_project=$wandb_project task=LeapCubeRotateZAxis seed=$seed impl=warp sampler_visualization=true
         done
     done
