2f6d7350407173b1bba3cbf3e07012c780fd6130
sukchul <tjrcjf410@snu.ac.kr>
2026-03-31 15:31:41 +0900

    asdf

--- learning/commands/exp_rotate_flow.sh
+++ learning/commands/exp_rotate_flow.sh
@@ -1,4 +1,4 @@
-wandb_project="rotate-sampler"
+wandb_project="rotate-sampler2"
 for seed in 0 1 2 3 4 5 6 7
     do
     python run.py policy=flowppo beta=5 wandb_project=$wandb_project task=LeapCubeRotateZAxis seed=$seed impl=warp sampler_visualization=true
